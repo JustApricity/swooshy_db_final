44875443c2279f8d8e354fc7a4442a444dbbe447
Apricity <101598622+JustApricity@users.noreply.github.com>
2023-04-26 12:05:44 -0700

    first commit

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,3 +1,38 @@
-let age = ['young', 'adult', 'elder']
-let personality = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-let location = []
+const {Animal} = require('../models');
+let ages = ['young', 'adult', 'elder'];
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let types = ['avian', 'reptilian', 'aquatic', 'arboreal', 'nocturnal', 'scavenger'];
+
+module.exports.renderAddForm = async function(req, res) {
+    const animal = {
+        color: '',
+        type: '',
+        age: '',
+    };
+    res.render('animals/add', {animal, ages, colors, types});
+};
+
+module.exports.addAnimal = async function (req,res) {
+    const animal = await Animal.create({
+        color: req.body.color,
+        type: req.body.type,
+        age: req.body.age,
+        user_id: 1, // todo get logged in
+        created_on: new Date()
+    });
+    res.redirect('/') // todo change the redirect to view all once made
+}
+
+module.exports.displayAnimal = async function(req, res){
+    const animal = await Animal.findByPk(req.params.animalId, {
+        include: ['user']
+    })
+    res.render('animals/view', {animal});
+}
+
+module.exports.displayAll = async function (req, res) {
+    const animals = await Animal.findAll({
+        include: ['user']
+    });
+    res.render('animals/viewAll', {animals});
+}
---
+++ models/animal.js
@@ -0,0 +1,41 @@
+'use strict';
+const moment = require('moment')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Animal extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Animal.belongsTo(models.User, {
+        as: 'user',
+        foreignKey: 'user_id'
+      })
+    }
+  }
+  Animal.init({
+    color: DataTypes.STRING,
+    type: DataTypes.STRING,
+    age: DataTypes.STRING,
+    image_url: DataTypes.STRING,
+    user_id: DataTypes.INTEGER,
+    created_on: DataTypes.DATE,
+    friendlyCreatedDate: {
+      type: DataTypes.VIRTUAL,
+      get(){
+        return moment(this.created_on).format('MMMM Do, YYYY')
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Animal',
+    timestamps: false,
+    tableName: 'swooshy_animals'
+  });
+  return Animal;
+};
